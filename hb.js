6b6dbbb1739aaf19322cd4d1a16a604dc95c64c6
savvnnk <jsd04052007@gmail.com>
2025-02-22 23:30:55 +0200

    Add files via upload

---
+++ hb.js
@@ -0,0 +1,4 @@
+document.getElementById("showGif").addEventListener("click", function() {
+    document.getElementById("GifContainer").style.display = "block";
+    document.getElementById("sound").play();
+});
